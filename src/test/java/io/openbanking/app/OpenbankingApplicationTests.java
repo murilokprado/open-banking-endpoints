9369def022aa810e16b80f15cb6a260476a406e5
Murilo Prado <kruzprado@hotmail.com>
2021-04-28 08:23:27 -0300

    WIP: adding controller test and create a response of api family types

--- src/test/java/io/openbanking/app/OpenbankingApplicationTests.java
+++ src/test/java/io/openbanking/app/OpenbankingApplicationTests.java
@@ -2,12 +2,13 @@ package io.openbanking.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootTest
+@EnableFeignClients
 class OpenbankingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
